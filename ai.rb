6b369977678945c2c42dfcdc9ea70ca9b4b1270a
km121500 <km121500@gmail.com>
2022-02-09 14:06:05 +0000

    幸運数

--- ai.rb
+++ ai.rb
@@ -53,3 +53,21 @@ end
 
 print((0..9).to_a.map{|v| fib(0,1,v)}) # フィボナッチ数を10個表示
 print((0..9).to_a.map{|v| fib(2,1,v)}) # リュカ数を10個表示
+
+def flt(a, i)
+  b = [0]+a
+  s = b[i]
+  l = b.length
+  if s <= l then
+    c = (0..(l-1)).to_a.select{|v| v%s != 0}.map{|v| b[v]}
+    return flt(c, i+1)
+  else
+    return a
+  end
+end
+
+def luc(n)
+  return flt((0..n).to_a.select{|x| x%2 != 0}, 2)
+end
+
+print(luc(100)) # 100までの幸運数を表示

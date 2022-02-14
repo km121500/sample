4f56b3cdb2c5027ea3d2d1ac94f36a036e3f33a6
km121500 <km121500@gmail.com>
2022-02-14 13:45:27 +0000

    commit

--- a.rb
+++ a.rb
@@ -188,3 +188,18 @@ def divisor_string_r(n, div, string)
     divisor_string_r(n, div+1, string)
   end
 end
+
+# sum3.rb
+# n から m までを足す関数 (for 文)
+# 計算の途中経過を表示する．
+
+def sum3(n, m)
+  s = 0
+  for i in n..m  # i を n から m まで 1 ずつ増やして以下の計算を行う．
+    s = s + i
+    print(s, "\n")
+    # 結果を表示
+    # s の値を表示して，改行 "\n" で指示
+  end
+  s
+end

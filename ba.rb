6fb16555d1a3b7c5e6b8d650a171ebebb4db3631
km121500 <km121500@gmail.com>
2022-03-05 12:54:05 +0000

    commit

--- ba.rb
+++ ba.rb
@@ -15,3 +15,26 @@ def luc(n)
 end
 
 print(luc(100)) # 100までの幸運数を表示
+
+
+def sieve(a,i,f)
+  b = a.select{|x| x<=Math.sqrt(i)}.map{|x| i%x != 0}.all?
+  if (b) then
+    a = a + [i]
+    if a.select{|x| x==(i-f)}.length>0 then
+      return a
+    end
+  end
+  return sieve(a,i+1,f)
+end
+
+def twin(f,c)
+  return (0..(c-1)).to_a.inject([[2],[]]){|a,_|
+    b = sieve(a[0],a[0].last+1,f)
+    [b,a[1]+[b.last]]
+  }[1].map{|x| [x-f,x]}
+end
+
+print(twin(2,10)) # 双子素数を10組表示
+print(twin(4,10)) # いとこ素数を10組表示
+print(twin(6,10)) # セクシー素数を10組表示

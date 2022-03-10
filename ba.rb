8eb0fe53e8cf99374c7b83b58914ea119f59419d
km121500 <km121500@gmail.com>
2022-03-10 13:29:00 +0000

    commit

--- ba.rb
+++ ba.rb
@@ -57,3 +57,28 @@ while i <= 10
   i += 1
 end
 print array.join(" ")
+
+
+require 'benchmark'
+
+n = 35
+Benchmark.bm(7) do |x|
+  x.report("memo") {
+    # 修正前(メモがメソッドの定義内なので効果なし)
+    def fibonacci(input)
+      memo = { 1=> 1, 2=> 1 }
+      return memo[input] if memo.has_key?(input)
+      memo[input] = fibonacci(input - 2) + fibonacci(input - 1)
+    end
+    fibonacci(n)
+  }
+  x.report("@memo") {
+    # 修正後(メモがメソッドのスコープ外なので効果あり)
+    @memo = { 1=> 1, 2=> 1 }
+    def fibonacci(input)
+      return @memo[input] if @memo
+      @memo[input] = fibonacci(input - 2) + fibonacci(input - 1)
+    end
+    fibonacci(n)
+  }
+end
